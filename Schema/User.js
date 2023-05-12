6832da099e182849449d69d5630fa0acc2566b02
Ali Husnain <ali.husnain@ktechz.net>
2023-05-12 16:22:11 +0500

    first commit

---
+++ Schema/User.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password'] })
+
+module.exports = mongoose.model("User", userSchema);
